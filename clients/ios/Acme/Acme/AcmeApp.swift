c2a576517f69108e96e8d9f567806b58efbe8a88
Miguel Fermin <mfermin@mafsoftware.com>
2021-12-11 16:58:00 -0500

    added plain xcode project

---
+++ clients/ios/Acme/Acme/AcmeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AcmeApp.swift
+//  Acme
+//
+//  Created by Miguel Fermin on 12/11/21.
+//
+
+import SwiftUI
+
+@main
+struct AcmeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
