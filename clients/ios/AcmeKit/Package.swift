0be3ddfec387a4ab31d0118b9196fff9c49e2a6a
Miguel Fermin <mfermin@mafsoftware.com>
2021-12-11 19:22:33 -0500

    removed default comments

--- clients/ios/AcmeKit/Package.swift
+++ clients/ios/AcmeKit/Package.swift
@@ -8,7 +8,6 @@ let package = Package(
     defaultLocalization: "en",
     platforms: [.iOS(.v15)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(name: "AcmeKit", targets: ["AcmeKit"]),
         
         .library(name: "AcmeAccount", targets: ["AcmeAccount"]),
@@ -20,16 +19,9 @@ let package = Package(
         .library(name: "AcmeUI", targets: ["AcmeUI"]),
         .library(name: "AcmeCore", targets: ["AcmeCore"]),
     ],
-    
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
     ],
-    
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        
         // MARK: - Umbrella module
         .target(
             name: "AcmeKit",
